f57e4af7b054b26b0db4cf1d7dc3a6cc3827b8f7
Juan Freydell <juanjfreydell@gmail.com>
2026-01-11 20:22:55 -0500

    fixing double search button bug

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -314,29 +314,6 @@ export default function Header() {
               href="#"
               onClick={(e) => {
                 e.preventDefault();
-                router.push("/");
-              }}
-              className="flex flex-col items-center justify-center py-2 px-3 min-w-[60px]"
-            >
-              <svg
-                className="h-6 w-6 text-black dark:text-white mb-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-              <span className="text-xs text-gray-500 dark:text-gray-400">Buscar</span>
-            </a>
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
                 if (user) {
                   router.push("/listar");
                 } else {
