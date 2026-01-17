6149a5d222890b90e376a06af81b0380d0b7e14e
Juan Jose Freydell <juanjfreydell@gmail.com>
2026-01-17 13:30:31 -0500

    Adding a more high res provider for maps

--- app/components/MapPicker.tsx
+++ app/components/MapPicker.tsx
@@ -140,8 +140,13 @@ export default function MapPicker({
           className="z-0"
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            subdomains="abcd"
+            maxZoom={20}
+            minZoom={1}
+            // Use higher quality tiles with retina support
+            detectRetina={true}
           />
           <Marker
             position={position}
